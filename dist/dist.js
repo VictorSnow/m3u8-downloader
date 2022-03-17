409d136970ff54e4c5c8fb81bbe561587f33540b
xl <m@victorup.com>
2022-03-17 11:52:51 +0800

    调整header

--- dist/dist.js
+++ dist/dist.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         m3u8-downloader
-// @namespace    https://github.com/Momo707577045/m3u8-downloader
+// @homepage     https://github.com/Momo707577045/m3u8-downloader
+// @namespace    https://github.com/VictorSnow/m3u8-downloader
 // @version      0.6.0
 // @description  M3U8 downloader
 // @downloadURL  https://raw.githubusercontent.com/VictorSnow/m3u8-downloader/main/dist/dist.js
